cff72676c22b7dc9e859c808fdb930e965379e68
David Whitlock <david.m.whitlock@gmail.com>
2025-07-07 19:46:58 -0700

    Implement more FizzBuzz logic.

--- FizzBuzz/src/main/java/edu/pdx/cs/joy/everyone/FizzBuzz.java
+++ FizzBuzz/src/main/java/edu/pdx/cs/joy/everyone/FizzBuzz.java
@@ -16,8 +16,18 @@ public class FizzBuzz {
   }
 
   public static String getFizzBuzzValue(int number) {
-    if (number % 3 == 0) {
+    if (number < 0) {
+      throw new IllegalArgumentException("Number must be greater than 0");
+    }
+
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+
+    } else if (number % 3 == 0) {
       return "Fizz";
+
+    } else if (number % 5 == 0) {
+      return "Buzz";
 
     } else {
       return String.valueOf(number);
--- FizzBuzz/src/test/java/edu/pdx/cs/joy/everyone/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/edu/pdx/cs/joy/everyone/FizzBuzzTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FizzBuzzTest
 {
@@ -21,6 +22,26 @@ public class FizzBuzzTest
   @Test
   void printThirdFizzBuzzValue() {
     assertThat(FizzBuzz.getFizzBuzzValue(3), equalTo("Fizz"));
+  }
+
+  @Test
+  void printFiveFizzBuzzValue() {
+    assertThat(FizzBuzz.getFizzBuzzValue(5), equalTo("Buzz"));
+  }
+
+  @Test
+  void printFifteenFizzBuzzValue() {
+    assertThat(FizzBuzz.getFizzBuzzValue(15), equalTo("FizzBuzz"));
+  }
+
+  @Test
+  void printZeroFizzBuzzValue() {
+    assertThat(FizzBuzz.getFizzBuzzValue(0), equalTo("FizzBuzz"));
+  }
+
+  @Test
+  void negativeFizzBuzzValue() {
+    assertThrows(IllegalArgumentException.class, () -> FizzBuzz.getFizzBuzzValue(-1));
   }
 
 }
