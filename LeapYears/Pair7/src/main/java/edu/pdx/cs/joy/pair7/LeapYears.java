b6c169c2cec7123e6fcecc7c4ab1e5a8e8a04d77
dgarciajoya <dgarciajoya4@gmail.com>
2025-07-09 19:29:18 -0700

    Worked on Display

--- LeapYears/Pair7/src/main/java/edu/pdx/cs/joy/pair7/LeapYears.java
+++ LeapYears/Pair7/src/main/java/edu/pdx/cs/joy/pair7/LeapYears.java
@@ -8,24 +8,27 @@ import com.google.common.annotations.VisibleForTesting;
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
-public static class LeapYears {
+public class LeapYears {
 
   @VisibleForTesting
   public static void main(String[] args) {
       if (args.length > 0) {
+
           try{
               LeapYears leapyear = new LeapYears();
               int year = Integer.parseInt(args[0]);
               String result = leapyear.isLeapYear(year);
               leapyear.displayResult(year, result);
           }
+
           catch (NumberFormatException e){
                 System.err.println("Invalid year format. Please enter a valid integer.");
                 return;
           }
+
           }
       }
-  }
+
 
     public static String isLeapYear(int i) {
       String s = "";
@@ -62,3 +65,4 @@ public static class LeapYears {
 
 
 
+}
