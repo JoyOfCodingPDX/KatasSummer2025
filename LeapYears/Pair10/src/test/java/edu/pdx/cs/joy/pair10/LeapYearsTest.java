acfd4c2487edc3788beff184ccc1758c6af5634b
hhk63 <hhk2@pdx.edu>
2025-07-09 18:38:48 -0700

    first round

--- LeapYears/Pair10/src/test/java/edu/pdx/cs/joy/pair10/LeapYearsTest.java
+++ LeapYears/Pair10/src/test/java/edu/pdx/cs/joy/pair10/LeapYearsTest.java
@@ -2,12 +2,27 @@ package edu.pdx.cs.joy.pair10;
 
 import org.junit.jupiter.api.Test;
 
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LeapYearsTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new LeapYears();
+  }
+
+  @Test
+  void checkCLIAndIsDivBy4(){
+
+    String arg = "5";
+    int expected = Integer.parseInt(arg);
+    assertEquals(expected, Integer.class);
+
   }
 
 }
