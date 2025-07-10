64357ac80a019c074e881bcffc996d44b7ea454a
Arturo Moreno <arturo9@pdx.edu>
2025-07-09 18:39:19 -0700

    Create a Test and method for identifying leap year

--- LeapYears/Pair7/src/test/java/edu/pdx/cs/joy/pair7/LeapYearsTest.java
+++ LeapYears/Pair7/src/test/java/edu/pdx/cs/joy/pair7/LeapYearsTest.java
@@ -2,12 +2,17 @@ package edu.pdx.cs.joy.pair7;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.function.Predicate.isEqual;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class LeapYearsTest
 {
 
   @Test
-  void canInstantiateKataClass() {
-    new LeapYears();
+  void checkifdividsibleBy4()
+  {
+    LeapYears leapYears = new LeapYears();
+    assertThat(leapYears.isLeapYear(2025), isEqual("is not a leap year"));
   }
-
 }
+
