6fb69f69d355b8ed29df1cd1ebdf7e0e08b8b42b
Arturo Moreno <arturo9@pdx.edu>
2025-07-09 19:18:27 -0700

    Added some command handling

--- LeapYears/Pair7/src/main/java/edu/pdx/cs/joy/pair7/LeapYears.java
+++ LeapYears/Pair7/src/main/java/edu/pdx/cs/joy/pair7/LeapYears.java
@@ -8,11 +8,23 @@ import com.google.common.annotations.VisibleForTesting;
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
-public class LeapYears {
+public static class LeapYears {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+      if (args.length > 0) {
+          try{
+              LeapYears leapyear = new LeapYears();
+              int year = Integer.parseInt(args[0]);
+              String result = leapyear.isLeapYear(year);
+              leapyear.displayResult(year, result);
+          }
+          catch (NumberFormatException e){
+                System.err.println("Invalid year format. Please enter a valid integer.");
+                return;
+          }
+          }
+      }
   }
 
     public static String isLeapYear(int i) {
@@ -44,11 +56,8 @@ public class LeapYears {
             }
 
       }*/
-    public static void Display(String[] args){
-                System.out.println(isLeapYear(100));
-
-    }
-
+    public static void displayResult(int year, String result) {
+        System.out.println("Year " + year + " " + result);
     }
 
 
