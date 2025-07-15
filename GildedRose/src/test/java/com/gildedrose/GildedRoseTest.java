245c8e4860ecab5871acc0c86f60dcd803dcdecb
David Whitlock <david.m.whitlock@gmail.com>
2025-07-14 18:50:21 -0700

    Refactor unit tests to remove duplicate code.z

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,30 +9,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        String itemName = "foo";
-        Item[] items = new Item[] { new Item(itemName, 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].name, equalTo(itemName));
-    }
+  @Test
+  void foo() {
+    String itemName = "foo";
+    Item[] items = new Item[]{new Item(itemName, 0, 0)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertThat(app.items[0].name, equalTo(itemName));
+  }
 
-    @Test
-    void qualityOfNormalItemDecreasesByOne() {
-        Item[] items = new Item[] { new Item("Normal Item", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].sellIn, equalTo(9));
-        assertThat(app.items[0].quality, equalTo(19));
-    }
+  @Test
+  void qualityOfNormalItemDecreasesByOne() {
+    Item item = updateQuality(new Item("Normal Item", 10, 20));
+    assertThat(item.sellIn, equalTo(9));
+    assertThat(item.quality, equalTo(19));
+  }
 
-    @Test
-    void qualityOfAgedBrieIncreasesByOne() {
-        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].sellIn, equalTo(1));
-        assertThat(app.items[0].quality, equalTo(1));
-    }
+  private static Item updateQuality(Item originalItem) {
+    Item[] items = new Item[]{originalItem};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    return app.items[0];
+  }
+
+  @Test
+  void qualityOfAgedBrieIncreasesByOne() {
+    Item item = updateQuality(new Item("Aged Brie", 2, 0));
+    assertThat(item.sellIn, equalTo(1));
+    assertThat(item.quality, equalTo(1));
+  }
 }
