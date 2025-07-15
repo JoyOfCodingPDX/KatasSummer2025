0788543a45b92a43a4f34e62fad1b92102fa6abb
David Whitlock <david.m.whitlock@gmail.com>
2025-07-14 18:45:46 -0700

    Start adding unit tests for the GildedRose.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,17 +3,36 @@ package com.gildedrose;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Disabled // This needs to be fixed
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        String itemName = "foo";
+        Item[] items = new Item[] { new Item(itemName, 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(app.items[0].name, equalTo(itemName));
     }
 
+    @Test
+    void qualityOfNormalItemDecreasesByOne() {
+        Item[] items = new Item[] { new Item("Normal Item", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].sellIn, equalTo(9));
+        assertThat(app.items[0].quality, equalTo(19));
+    }
+
+    @Test
+    void qualityOfAgedBrieIncreasesByOne() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].sellIn, equalTo(1));
+        assertThat(app.items[0].quality, equalTo(1));
+    }
 }
