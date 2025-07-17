deb521795fb9852710ccdffc93c67462898b452f
Sam Burich <burich@pdx.edu>
2025-07-16 19:31:25 -0700

    added tests for output first row

--- Diamond/Pair10/src/main/java/edu/pdx/cs/joy/pair10/Diamond.java
+++ Diamond/Pair10/src/main/java/edu/pdx/cs/joy/pair10/Diamond.java
@@ -10,6 +10,25 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class Diamond {
 
+  private final int letterValue;
+
+  public Diamond(char letter) {
+    letterValue = (int) letter - 64;
+  }
+
+  public String outputDiamond() {
+    int length = (letterValue * 2) - 1;
+    String result = "";
+    for(int i = 0; i < length; i++) {
+      if(i == length/2) {
+        result = result + "A";
+      } else {
+        result = result + " ";
+      }
+    }
+    return result;
+  }
+
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
