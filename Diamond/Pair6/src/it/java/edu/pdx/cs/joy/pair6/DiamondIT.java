f21e4946d6c55396441e70794521237e6b1328c3
Jayita Banerjee <jbanerje@pdx.edu>
2025-07-16 20:14:11 -0700

    Added  one integration test   for Pair 6

--- Diamond/Pair6/src/it/java/edu/pdx/cs/joy/pair6/DiamondIT.java
+++ Diamond/Pair6/src/it/java/edu/pdx/cs/joy/pair6/DiamondIT.java
@@ -3,6 +3,7 @@ package edu.pdx.cs.joy.pair6;
 import edu.pdx.cs.joy.InvokeMainTestCase;
 import org.junit.jupiter.api.Test;
 
+import static java.util.function.Predicate.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
 
@@ -23,5 +24,16 @@ class DiamondIT extends InvokeMainTestCase {
                     " A\n";
     assertThat(result.getTextWrittenToStandardOut(), containsString(expected));
   }
+
+  @Test
+  void mainWithInvalidCharacterPrintsError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Diamond.class, "1");
+    assertThat(result.getTextWrittenToStandardError(), containsString("Input must be a letter"));
+  }
+
+
+
+
+
 
 }
