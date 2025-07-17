0585f35e9f08351099bab5c8ad046fa7917e4d91
Gil Tighe <gtighe@pdx.edu>
2025-07-16 19:43:12 -0700

    huh?

--- Diamond/Pair13/src/test/java/edu/pdx/cs/joy/pair13/DiamondTest.java
+++ Diamond/Pair13/src/test/java/edu/pdx/cs/joy/pair13/DiamondTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DiamondTest
 {
@@ -11,8 +12,22 @@ public class DiamondTest
   @Test
   void canInstantiateKataClass() {
     Diamond displayDiamond = new Diamond('C');
-
     assertThat(displayDiamond.character, equalTo('C'));
+  }
+  @Test
+  void thatDiamondReturnsNumericalValueOfCharacter() {
+    Diamond displayDiamond = new Diamond('C');
+    assertThat(displayDiamond.getNumber(), equalTo(3));
+  }
+  @Test
+    void thatDiamondReturnsNumericalValueOfLowerCaseCharacter() {
+        Diamond displayDiamond = new Diamond('c');
+        assertThat(displayDiamond.getNumber(), equalTo(3));
+    }
+  @Test
+  void cannotUseInvalidCharacter() {
+    Diamond displayDiamond = new Diamond('1');
+    assertThrows(IllegalArgumentException.class, displayDiamond::getNumber);
   }
 
 }
