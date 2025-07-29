a45935bc2fe0d9186837a9de7c28a6ff5534658f
Sam Burich <burich@pdx.edu>
2025-07-28 19:26:41 -0700

    created Game class and first test

---
+++ Tennis/src/main/java/edu/pdx/cs/joy/mob/Game.java
@@ -0,0 +1,24 @@
+package edu.pdx.cs.joy.mob;
+
+import com.google.common.annotations.VisibleForTesting;
+
+/**
+ * A class for getting started with a code kata
+ *
+ * Use IntelliJ's "Refactor | Rename..." command to change the name of this
+ * class (and its tests).
+ */
+public class Game {
+
+  private int p1Score = 0;
+  private int p2Score = 0;
+
+  public int getP1Score() {
+    return p1Score;
+  }
+
+  public int getP2Score() {
+    return p2Score;
+  }
+
+}
--- Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
+++ Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
@@ -2,12 +2,28 @@ package edu.pdx.cs.joy.mob;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TennisTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Tennis();
+  }
+
+  @Test
+  void startGameAtLoveAll() {
+    Game g = new Game();
+    assertEquals(g.getP1Score(), 0);
+    assertEquals(g.getP2Score(), 0);
+  }
+
+  @Test
+  void testToString() {
+    Game g = new Game();
+    // assertEquals(g.to
   }
 
 }
