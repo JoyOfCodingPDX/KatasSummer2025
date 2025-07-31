06118627ebd26c28f72b3c685244e4f922979a04
Arturo Moreno <arturo9@pdx.edu>
2025-07-30 19:21:56 -0700

    Made main

--- RPNCalculator/Mob4/src/main/java/edu/pdx/cs/joy/mob4/RPNCalculator.java
+++ RPNCalculator/Mob4/src/main/java/edu/pdx/cs/joy/mob4/RPNCalculator.java
@@ -6,23 +6,40 @@ import java.util.Stack;
 
 /**
  * A class for getting started with a code kata
- *
+ * <p>
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
 public class RPNCalculator {
-  public Stack<Double> numberStack = new Stack<>();
+    public Stack<Double> numberStack = new Stack<>();
 
-  public Double addition(String args[]){
-    // Adds values to stack
-    numberStack.push(Double.parseDouble(args[0]));
-    numberStack.push(Double.parseDouble(args[1]));
+    public Double addition(Double firstNumber, Double secondNumber) {
 
-    //Extracts values
-    Integer firstNumber = Integer.parseInt(args[0]);
-    Integer secondNumber = Integer.parseInt(args[1]);
+        return firstNumber + secondNumber;
+    }
 
-    return numberStack.pop();
-  }
+    public static void main(String[] args) {
+        RPNCalculator calculator = new RPNCalculator();
+
+        // Parse Command Line Arguments
+        for (String arg : args) {
+            switch (arg) {
+                case "+":
+                    calculator.numberStack.push(
+                            calculator.addition(calculator.numberStack.pop(), calculator.numberStack.pop()));
+                    break;
+                default:
+                    try {
+                        // Try to parse the argument as a number
+                        Double number = Double.parseDouble(arg);
+                        calculator.numberStack.push(number);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input: " + arg);
+                    }
+            }
+        }
+    }
+
+}
 
 }
