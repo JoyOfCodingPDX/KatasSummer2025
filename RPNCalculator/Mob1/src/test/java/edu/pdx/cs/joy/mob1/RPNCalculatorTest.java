2832dda3d19a7a825ec812b194f9a65688cd4cf0
SherazSAhmed <159236039+SherazSAhmed@users.noreply.github.com>
2025-07-30 19:11:11 -0700

    Push 2

--- RPNCalculator/Mob1/src/test/java/edu/pdx/cs/joy/mob1/RPNCalculatorTest.java
+++ RPNCalculator/Mob1/src/test/java/edu/pdx/cs/joy/mob1/RPNCalculatorTest.java
@@ -39,4 +39,12 @@ public class RPNCalculatorTest {
     int result = rpn.evaluate("20 5 /");
     assertThat(result, equalTo(4));
   }
+
+  @Test
+  void TooFewArguments(){
+    RPNCalculator rpn = new RPNCalculator();
+    int result = rpn.evaluate("20");
+    assertThat(result., equalTo(0));
+
+  }
 }
